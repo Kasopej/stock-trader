b2a30f74d210ba791b98faa3913a9c69f27ea67c
Kasope Johnson <kasopej@gmail.com>
2022-02-18 13:25:19 +0100

    added skeleton for navigation header

---
+++ src/components/Header.vue
@@ -0,0 +1,16 @@
+<template>
+  <nav class="flex justify-content-between">
+      <div class="px-2 bg-secondary"></div>
+      <div class="px-2 bg-secondary"></div>
+  </nav>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
