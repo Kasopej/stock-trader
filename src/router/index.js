869c839118b5a7dc3dc03b015ac9fb094a9be23c
Kasope Johnson <kasopej@gmail.com>
2022-02-21 12:47:20 +0100

    Created routes and skeletal views for portfolio & stocks

--- src/router/index.js
+++ src/router/index.js
@@ -19,6 +19,24 @@ const routes = [
     component: () =>
       import(/* webpackChunkName: "about" */ "../views/About.vue"),
   },
+  {
+    path: "/stocks",
+    name: "stocks",
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () =>
+      import(/* webpackChunkName: "about" */ "../views/StocksView.vue"),
+  },
+  {
+    path: "/portfolio",
+    name: "portfolio",
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () =>
+      import(/* webpackChunkName: "about" */ "../views/Portfolio.vue"),
+  },
 ];
 
 const router = new VueRouter({
---
+++ src/views/StocksView.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="container-fluid">
+      <Stock v-for="stock in stocks" :key="stock.id"></Stock>
+  </div>
+</template>
+
+<script>
+import Stock from "@/components/Stock.vue";
+export default {
+    data() {
+        return {
+            stocks: []
+        }
+    },
+    components: {
+        Stock,
+    }
+}
+</script>
+
+<style>
+    
+</style>
