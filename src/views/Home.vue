168109be63acdbeb8b110bb48abd9c2d3677108f
Kasope Johnson <kasopej@gmail.com>
2022-02-21 16:02:57 +0100

    Designs basic look of panes on home page

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,5 +1,26 @@
 <template>
-  <div class="home"></div>
+  <div class="home d-flex flex-wrap justify-content-lg-between justify-content-around px-5 py-3">
+    <div class="homePane border rounded my-4">
+      <p class="lead">Your Balance</p>
+      <h2>${{125000}}</h2>
+      <p class="lead mb-4">of your $1,200 goal</p>
+      <button class="btn btn-success btn-md rounded-pill">Adjust Goal</button>
+    </div>
+
+    <div class="homePane border rounded my-4">
+      <p class="lead">Your Balance</p>
+      <h2>${{125000}}</h2>
+      <p class="lead mb-4">of your $1,200 goal</p>
+      <button class="btn btn-success btn-md rounded-pill">Adjust Goal</button>
+    </div>
+
+    <div class="homePane border rounded my-4">
+      <p class="lead">Your Balance</p>
+      <h2>${{125000}}</h2>
+      <p class="lead mb-4">of your $1,200 goal</p>
+      <button class="btn btn-success btn-md rounded-pill">Adjust Goal</button>
+    </div>
+  </div>
 </template>
 
 <script>
@@ -10,3 +31,8 @@ export default {
   components: {},
 };
 </script>
+<style scoped>
+  .homePane{padding: 35px 55px; flex-basis: 32%; text-align: center;}
+  .homePane h2{font-size: 45px;}
+  .homePane p {font-size: 15px;}
+</style>
