c649a4123f906d07c4ca7efc90b78fa6691c9fd7
Kasope Johnson <kasopej@gmail.com>
2022-02-25 12:08:13 +0100

    Removed example test script & added landing section for unauthenticated users

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,5 +1,43 @@
 <template>
-  <div
+  <div v-if="!userAuthenticated">
+            <section class="p-5 bg-primary text-white mb-0" id="about">
+                <div class="container">
+                    <h2 class="page-section-heading text-center text-uppercase text-white">About</h2>
+                    <div class="row">
+                        <div class="col-12 ms-auto text-center"><p class="lead mb-1">Welcome to your personal stock trading app</p>
+                        <p class="lead">Use this application to buy shares, manage your portfolio, and grow your wealth!</p></div>
+                    </div>
+                </div>
+            </section>
+
+            <!-- Contact Section-->
+            <section class="p-5" id="contact">
+                <div class="container">
+                    <h2 class="text-center text-uppercase text-secondary mb-2">Login</h2>
+                    <div class="row justify-content-center">
+                        <div class="col-lg-8 col-xl-7">
+                            <form id="contactForm">
+                                <div class="form-floating mb-3">
+                                    <input class="form-control" id="email" type="email" placeholder="Enter your email..." />
+                                    <label for="email">email</label>
+                                    <div class="invalid-feedback">A name is required.</div>
+                                </div>
+                                <div class="form-floating mb-3">
+                                    <input class="form-control" id="password" type="password"/>
+                                    <label for="password">Password</label>
+                                    <div class="invalid-feedback">A password is required.</div>
+                                </div>
+                                <button class="btn btn-primary btn-xl disabled" id="submitButton" type="submit">Send</button>
+                            </form>
+                            <div class="mt-3">
+                                <p>Don't have an account?</p><router-link to="/register">Register</router-link>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </section>
+        </div>
+  <div v-else
     class="home d-flex flex-wrap justify-content-lg-between justify-content-around px-5 py-3"
   >
     <div
@@ -50,7 +88,9 @@
 export default {
   name: "Home",
   data() {
-    return {};
+    return {
+      userAuthenticated: true
+    };
   },
   components: {},
   methods: {
