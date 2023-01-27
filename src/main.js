7e9e2eaea67b4f9e763e2376f9caa394fb9b857a
Kasope Johnson <kasopej@gmail.com>
2023-01-27 16:19:51 +0100

    chore: uses webpack via vue-cli for bundling bootstrap replacing gulp

--- src/main.js
+++ src/main.js
@@ -1,9 +1,12 @@
 import Vue from "vue";
+// import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store/index";
 import SignOutModal from "./components/UtilityComponents/SignOutModal.vue";
 //Vue.config.productionTip = false;
+
+import "@/assets/scss/custom.scss";
 
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
